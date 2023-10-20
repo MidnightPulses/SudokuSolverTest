098b7aa4cb374c272cee5828603460f4c9967418
Filip Kotlaja <Dunja Computers@DESKTOP-IBMR4S4>
2023-10-20 22:51:52 +0200

    test

--- SudokuSolverApp/src/BoardAndReader.java
+++ SudokuSolverApp/src/BoardAndReader.java
@@ -1,4 +1,27 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class BoardAndReader {
-
+	
+	private int[][] board;
+	private int boardsize;
+	
+	public BoardAndReader(int boardsize) {
+		this.boardsize = boardsize;
+		this.board = new int[boardsize][boardsize];			
+		}
+	public void BoarderLoader(String filepath) {//it rhymes 
+		try (BufferedReader BfR = new BufferedReader(new FileReader(filepath))){
+			for (int row=0; row<boardsize; row++) {
+				String[]rowvalues = BfR.readLine().split("");
+				for(int coloumn =0; coloumn<boardsize; coloumn++) {
+					board[row][coloumn]=Integer.parseInt(rowvalues[coloumn]);
+				}
+			}
+			
+		} catch (IOException exc) {
+			exc.printStackTrace();
+		}
+	}
 }
