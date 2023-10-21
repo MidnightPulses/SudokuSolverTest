0bbfe18bd83fc6857ddbd0f94059d0b62fc8f325
Dunja Computers <Dunja Computers@DESKTOP-IBMR4S4>
2023-10-22 00:48:02 +0200

    output

--- SudokuSolverApp/src/Printer.java
+++ SudokuSolverApp/src/Printer.java
@@ -1,4 +1,6 @@
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Printer {
 
 	public static void PrintBoard(BoardAndReader sudokuboard) {
@@ -13,4 +15,21 @@ public class Printer {
 			System.out.println();
 		}
 	}
+	 public static void ExportFile(BoardAndReader sudokuboard, String outputpath) {
+	        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputpath))) {
+	            int[][] board = sudokuboard.getBoard();
+	            int size = sudokuboard.getSize();
+
+	            for (int row = 0; row < size; row++) {
+	                for (int column = 0; column < size; column++) {
+	                    writer.write(board[row][column] + " ");
+	                }
+	                writer.newLine();
+	            }
+
+	            System.out.println("Board saved to" + outputpath);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
